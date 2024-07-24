29b21d4f7bdb9c2aa575b283365f55d7ab62d1b2
Amarok <snowlyicewolf@outlook.com>
2024-07-24 20:38:20 +0800

    🐺☕📰🖊️

--- src/main/kotlin/club/someoneice/crystaldream/client/geo/model/GeoAltar.kt
+++ src/main/kotlin/club/someoneice/crystaldream/client/geo/model/GeoAltar.kt
@@ -1,12 +1,12 @@
 package club.someoneice.crystaldream.client.geo.model
 
-import club.someoneice.crystaldream.CrystalDream
 import club.someoneice.crystaldream.common.tile.TileAltar
+import club.someoneice.crystaldream.util.createModPath
 import net.minecraft.resources.ResourceLocation
 import software.bernie.geckolib.model.GeoModel
 
 class GeoAltar: GeoModel<TileAltar>() {
-    override fun getModelResource(animatable: TileAltar): ResourceLocation = ResourceLocation(CrystalDream.MODID, "geo/magic_altar.geo.json")
-    override fun getTextureResource(animatable: TileAltar): ResourceLocation = ResourceLocation(CrystalDream.MODID, "textures/geo/magic_altar.png")
-    override fun getAnimationResource(animatable: TileAltar): ResourceLocation = ResourceLocation(CrystalDream.MODID, "animations/magic_altar.animation.json")
+    override fun getModelResource(animatable: TileAltar): ResourceLocation = createModPath("geo/magic_altar.geo.json")
+    override fun getTextureResource(animatable: TileAltar): ResourceLocation = createModPath("textures/geo/magic_altar.png")
+    override fun getAnimationResource(animatable: TileAltar): ResourceLocation = createModPath("animations/magic_altar.animation.json")
 }
--- src/main/kotlin/club/someoneice/crystaldream/client/geo/model/GeoCrystalBall.kt
+++ src/main/kotlin/club/someoneice/crystaldream/client/geo/model/GeoCrystalBall.kt
@@ -1,12 +1,12 @@
 package club.someoneice.crystaldream.client.geo.model
 
-import club.someoneice.crystaldream.CrystalDream
 import club.someoneice.crystaldream.common.tile.TileCrystalBall
+import club.someoneice.crystaldream.util.createModPath
 import net.minecraft.resources.ResourceLocation
 import software.bernie.geckolib.model.GeoModel
 
 class GeoCrystalBall: GeoModel<TileCrystalBall>() {
-    override fun getModelResource(animatable: TileCrystalBall): ResourceLocation = ResourceLocation(CrystalDream.MODID, "geo/crystal_ball.geo.json")
-    override fun getTextureResource(animatable: TileCrystalBall): ResourceLocation = ResourceLocation(CrystalDream.MODID, "textures/geo/crystal_ball.png")
-    override fun getAnimationResource(animatable: TileCrystalBall): ResourceLocation = ResourceLocation(CrystalDream.MODID, "animations/crystal_ball.animation.json")
+    override fun getModelResource(animatable: TileCrystalBall): ResourceLocation = createModPath("geo/crystal_ball.geo.json")
+    override fun getTextureResource(animatable: TileCrystalBall): ResourceLocation = createModPath( "textures/geo/crystal_ball.png")
+    override fun getAnimationResource(animatable: TileCrystalBall): ResourceLocation = createModPath( "animations/crystal_ball.animation.json")
 }
