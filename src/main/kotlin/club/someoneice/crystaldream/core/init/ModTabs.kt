002d0794964de69e784bf97b94a3ccbaba650df6
Amarok <snowlyicewolf@outlook.com>
2024-09-05 18:27:53 +0800

    🐺🔧

--- src/main/kotlin/club/someoneice/crystaldream/core/init/ModTabs.kt
+++ src/main/kotlin/club/someoneice/crystaldream/core/init/ModTabs.kt
@@ -1,6 +1,7 @@
 package club.someoneice.crystaldream.core.init
 
 import club.someoneice.crystaldream.core.CrystalDream
+import club.someoneice.crystaldream.util.asStack
 import com.google.common.base.Supplier
 import net.minecraft.core.registries.BuiltInRegistries
 import net.minecraft.network.chat.Component
@@ -16,11 +17,11 @@ object ModTabs {
 
     @Suppress("Unused")
     val GROUP_MAIN: CreativeModeTab by TABS.register("main", Supplier<CreativeModeTab> {
-        CreativeModeTab.builder().noScrollBar()
+        CreativeModeTab.builder()
             .title(Component.translatable("crystaldream.tab"))
-            .icon { ModItems.SOUL_CRYSTAL.defaultInstance }
+            .icon { ModItems.CRYSTAL_BALL.defaultInstance }
             .displayItems { _, output ->
-                ModItems.ITEMS.entries.map(DeferredHolder<Item, out Item>::get).forEach(output::accept)
+                ModItems.ITEMS.entries.map(DeferredHolder<Item, out Item>::get).map(Item::asStack).forEach(output::accept)
             }.build()
     })
 }
